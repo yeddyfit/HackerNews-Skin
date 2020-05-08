fdf647846be174faa47f62dcf5de081fc6166d5d
Jacob Colyvan <jacob_colyvan@live.com>
2020-05-08 14:53:34 +1000

    centred registration screen

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ topStoriesURL = "https://hacker-news.firebaseio.com/v0/topstories.json?"
 topStories = HTTParty.get(topStoriesURL).parsed_response
 
 # returns url/titles for the first 10 top stories
-for i in 0..10 
+for i in 0..15 
   storyURL = "https://hacker-news.firebaseio.com/v0/item/#{topStories[i]}.json"
   storyJSON = HTTParty.get(storyURL).parsed_response
   Post.create(
