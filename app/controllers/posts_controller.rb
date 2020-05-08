daa5df0da4b761885217db2ca4ffa2e9ca40898e
Jacob Colyvan <jacob_colyvan@live.com>
2020-05-08 10:43:56 +1000

    Added post/user comment functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,7 +10,12 @@ class PostsController < ApplicationController
   end
 
   def createComment
-
+    @post = Post.find(params["id"])
+    @comment = current_user.comments.create(post_id: @post.id, commentText: params[:comment])
+    redirect_to post_path
   end
 
+  def comment_params
+    params.require(:comment).permit(:commentText)
+  end
 end
